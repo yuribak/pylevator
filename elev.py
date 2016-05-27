316f0e7f733d7cbd669c43e3ee2677b8e1e22b6a
yuribak <yuri.bakulin@rubikloud.com>
2016-05-27 01:46:44 -0400

    generating riders

--- elev.py
+++ elev.py
@@ -1,20 +1,20 @@
-import math
+
 import random
 import time
 
-from asciimatics.effects import Sprite
-from asciimatics.paths import Path
-from asciimatics.renderers import StaticRenderer
-from asciimatics.scene import Scene
 from asciimatics.screen import Screen
 
 SECOND = 1.
 
-floors = 10
-elev_count = 5
+
+floor_count = 10
+floors = [list() for _ in xrange(floor_count)]
+elev_count = 4
 elev_w = 5
 
-frame_w, frame_h, frame_x, frame_y = 30, floors + 2, 3, 3
+RPS = 3
+
+frame_w, frame_h, frame_x, frame_y = 30, floor_count + 2, 3, 3
 frame_x2, frame_y2 = frame_x + frame_w - 1, frame_y + frame_h - 1
 
 
@@ -25,12 +25,13 @@ class Elevator(object):
 
     def __init__(self):
         self.pos = 0
+        self.new_pos = 0
         self.load = 0
         self.state = Elevator.IDLE
-        self.target = random.randint(0, floors - 1)
-
+        self.target = random.randint(0, floor_count - 1)
 
 def main(s):
+
     def draw_rect(w, h, x, y, hchar='#', vchar='|'):
         # top
         s.move(x, y)
@@ -56,21 +57,25 @@ def main(s):
         draw_rect(elev_w, frame_h, frame_x2 - (elev_w - 1) * (i + 1), frame_y)
 
     def print_elevator(i, n, c='@'):
-        s.print_at(c, frame_x2 - (elev_w - 1) * (i + 1) + elev_w / 2, frame_x + frame_h - 2 - n)
+        s.print_at(c, frame_x2 - (elev_w - 1) * (i + 1) + elev_w / 2, frame_y + frame_h - 2 - n)
 
     def move_elevators(elevators):
 
         for i, e in enumerate(elevators):
-            if e.pos < e.target:
-                pos_d = 1
-            elif e.pos > e.target:
-                pos_d = -1
-            else:
-                pos_d = 0
+            e.pos = e.new_pos
 
-            print_elevator(i, e.pos, c=' ')
-            e.pos += pos_d
-            print_elevator(i, e.pos)
+            if e.pos < e.target:
+                e.new_pos = e.pos+1
+            elif e.pos > e.target:
+                e.new_pos = e.pos-1
+            else:
+                e.new_pos = e.pos
+
+    def generate_riders():
+        pool = range(floor_count)
+        for _ in xrange(RPS):
+            s,t = random.sample(pool, 2)
+            floors[s].append(t)
 
     elevators = [Elevator() for _ in xrange(elev_count)]
 
@@ -78,11 +83,25 @@ def main(s):
 
         current_frame = time.time()
 
+        # update game state
         move_elevators(elevators)
+        generate_riders()
+
+        # update display
+        for i, e in enumerate(elevators):
+            print_elevator(i, e.pos, c=' ')
+            print_elevator(i, e.new_pos)
+
+        for f in xrange(len(floors)):
+            m = str(len(floors[f]))
+            s.print_at(m, frame_x+2, frame_y + frame_h - 2 - f)
 
         s.refresh()
 
-        time.sleep(SECOND - (time.time() - current_frame))
+        d = SECOND - (time.time() - current_frame)
+        if d > 0:
+            time.sleep(d)
+
 
 
 Screen.wrapper(main)
