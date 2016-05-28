b0c4e15e7c496369cb8596b44da403b1ddba90d4
yuribak <yuri.bakulin@rubikloud.com>
2016-05-28 03:04:55 -0400

    refactored into classes, added basic stats

--- elev.py
+++ elev.py
@@ -1,25 +1,52 @@
-
 import random
 import time
+from functools import total_ordering
 
 from asciimatics.screen import Screen
 
-SECOND = 1.
+FRAME_W = 150
+ELEV_W = 15
+
+GAME_TIME = 2
+GAME_SECOND = .2
+RPS = 5
 
 
-floor_count = 8 
-floors = [list() for _ in xrange(floor_count)]
-elev_count = 2
-elev_w = 15
+@total_ordering
+class Rider(object):
+    produced = 0
+    delivered = 0
+    riding = 0
+    time_alive = 0.
+    time_riding = 0.
 
-RPS = 4
+    def __init__(self, target):
+        Rider.produced += 1
+        self.target = target
+        self.birth_time = time.time()
+        self.load_time = None
+        self.death = self.birth_time + 20 * GAME_SECOND
 
-frame_w, frame_h, frame_x, frame_y = 80, floor_count + 2, 3, 3
-frame_x2, frame_y2 = frame_x + frame_w - 1, frame_y + frame_h - 1
+    def __gt__(self, other):
+        return self.target > other.target if isinstance(other, Rider) else self.target > other
+
+    def __eq__(self, other):
+        return self.target == other.target if isinstance(other, Rider) else self.target == other
+
+    def load(self):
+        self.load_time = time.time()
+        Rider.riding += 1
+        pass
+
+    def unload(self):
+        Rider.delivered += 1
+        Rider.riding -= 1
+        t = time.time()
+        Rider.time_alive += t - self.birth_time
+        Rider.time_riding += t - self.load_time
 
 
 class Elevator(object):
-
     CAPACITY = 12
 
     IDLE = 3
@@ -27,22 +54,40 @@ class Elevator(object):
     SEND = 2
     FETCH = 5
 
-    def __init__(self):
-        self.pos = 0
+    def __init__(self, pos):
+        self.pos = pos
         self.old_pos = 0
         self.load = []
         self.state = Elevator.IDLE
-        self.target = random.randint(0, floor_count - 1)
+        self.target = 0
+        self.ctx = {}
 
     def __repr__(self):
         return '[{}>{}]'.format(len(self.load), self.target)
 
-    def idle(self):
+    # load/unload riders
 
+    def re_load(self, floor):
+        re_load = []
+        for r in self.load:
+            if r == self.pos:
+                r.unload()
+                continue
+            re_load.append(r)
+        self.load = re_load
+
+        d = self.CAPACITY - len(self.load)
+        for r in floor[:d]:
+            r.load()
+            self.load.append(r)
+
+        del floor[:d]
+
+    def idle(self):
         # position
         # list of waiting riders
         # other elevators?
-        return # (target)
+        return  # (target)
         pass
 
     def load(self):
@@ -55,103 +100,174 @@ class Elevator(object):
         pass
 
 
-def idle_sequential(e):
-    e.target = (e.pos+1)%floor_count
+def idle_sequential(e, floors):
+    e.target = (e.pos + 1) % floors
 
-def idle_random(e):
-    e.target = random.randint(0,len(floors)-1)
 
-idle = idle_random
+def idle_random(e, floors):
+    e.target = random.randint(0, len(floors) - 1)
 
-def main(s):
 
-    def draw_rect(w, h, x, y, hchar='#', vchar='|'):
-        # top
-        s.move(x, y)
-        s.draw(x + w, y, char=hchar)
+def idle_real(e, floors):
+    going_up = e.ctx.setdefault('going_up', True)
+    target = None
+    if going_up:
+        up = [r for r in e.load if r > e.pos]
+        if up:
+            target = min(up).target
+        else:
+            going_up = False
+    if not going_up:
+        down = [r for r in e.load if r < e.pos]
+        if down:
+            target = max(down).target
+        else:
+            going_up = True
+    if target is not None:
+        e.target = target
+        e.ctx['going_up'] = going_up
+    else:
+        waiting = [f for f in range(len(floors)) if floors[f]]
+        if waiting:
+            e.target = min(waiting, key=lambda x: abs(e.pos - x))
+            e.ctx['going_up'] = e.target > e.pos
 
-        # right
-        s.move(x + w - 1, y + 1)
-        s.draw(x + w - 1, y + h - 1, char=vchar)
 
-        # bottom
-        s.move(x + w - 1, y + h - 1)
-        s.draw(x - 1, y + h - 1, char=hchar)
+def draw_rect(screen, w, h, x, y, hchar='#', vchar='|'):
+    # top
+    screen.move(x, y)
+    screen.draw(x + w, y, char=hchar)
 
-        # left
-        s.move(x, y + h - 2)
-        s.draw(x, y, char=vchar)
+    # right
+    screen.move(x + w - 1, y + 1)
+    screen.draw(x + w - 1, y + h - 1, char=vchar)
 
-    # draw frame
-    draw_rect(frame_w, frame_h, frame_x, frame_y)
+    # bottom
+    screen.move(x + w - 1, y + h - 1)
+    screen.draw(x - 1, y + h - 1, char=hchar)
 
-    # draw elevator shafts
-    for i in xrange(elev_count):
-        draw_rect(elev_w, frame_h, frame_x2 - (elev_w - 1) * (i + 1), frame_y)
+    # left
+    screen.move(x, y + h - 2)
+    screen.draw(x, y, char=vchar)
 
-    def print_elevator(i, n, c='@', color=Screen.COLOUR_WHITE):
-        #s.print_at(c, frame_x2 - (elev_w - 1) * (i + 1) + elev_w / 2, frame_y + frame_h - 2 - n, color)
-        s.print_at(c, frame_x2 - (elev_w - 1) * (i + 1)+1, frame_y + frame_h - 2 - n, color)
 
-    def move_elevators(elevators):
+idle = idle_real
 
+
+class Display(object):
+    def __init__(self, screen, floor_count, elev_count):
+        self.screen = screen
+        self.floor_count = floor_count
+        self.elev_count = elev_count
+
+        self.frame_w, self.frame_h, self.frame_x, self.frame_y = FRAME_W, floor_count + 2, 3, 3
+        self.frame_x2, self.frame_y2 = self.frame_x + self.frame_w - 1, self.frame_y + self.frame_h - 1
+
+        # draw frame
+        draw_rect(self.screen, self.frame_w, self.frame_h, self.frame_x, self.frame_y)
+
+        # draw elevator shafts
+        for i in xrange(elev_count):
+            draw_rect(self.screen, ELEV_W, self.frame_h, self.frame_x2 - (ELEV_W - 1) * (i + 1), self.frame_y)
+
+    def print_elevator(self, i, n, c='@', color=Screen.COLOUR_WHITE):
+        # self.s.print_at(c, frame_x2 - (elev_w - 1) * (i + 1) + elev_w / 2, frame_y + frame_h - 2 - n, color)
+        self.screen.print_at(c, self.frame_x2 - (ELEV_W - 1) * (i + 1) + 1, self.frame_y + self.frame_h - 2 - n, color)
+
+    def update_display(self, elevators, floors):
+
+        # elevator positions
         for i, e in enumerate(elevators):
+            self.print_elevator(i, e.old_pos, c=' ' * (len(str(e))+2))
+            self.print_elevator(i, e.pos, c=str(e), color=e.state)
+
+        # rider counds
+        for f in xrange(len(floors)):
+            m = str(len(floors[f]))
+            self.screen.print_at('    ', self.frame_x + 2, self.frame_y + self.frame_h - 2 - f)
+            self.screen.print_at(m, self.frame_x + 2, self.frame_y + self.frame_h - 2 - f)
+
+        self.screen.refresh()
+
+
+class Game(object):
+    def __init__(self, screen, floor_count, elev_count):
+
+        self.display = Display(screen, floor_count, elev_count)
+
+        self.floors = [list() for _ in xrange(floor_count)]
+        self.elevators = [Elevator(random.randint(0, floor_count - 1)) for _ in xrange(elev_count)]
+
+    def move_elevators(self):
+
+        for i, e in enumerate(self.elevators):
             e.old_pos = e.pos
 
             if e.pos < e.target:
-                e.pos = e.old_pos+1
+                e.pos = e.old_pos + 1
             elif e.pos > e.target:
-                e.pos = e.old_pos-1
+                e.pos = e.old_pos - 1
             else:
                 e.pos = e.old_pos
 
-    def generate_riders():
-        pool = range(floor_count)
+    def generate_riders(self):
+        pool = range(len(self.floors))
         for _ in xrange(RPS):
-            s,t = random.sample(pool, 2)
+            s, t = random.sample(pool, 2)
 
-            floors[s].append(t)
+            self.floors[s].append(Rider(t))
 
-    elevators = [Elevator() for _ in xrange(elev_count)]
-
-    while True:
-
-        current_frame = time.time()
-
-        # update game state
-
-        for e in elevators:
+    def update_state(self):
+        # IDLE
+        for e in self.elevators:
             if e.state == Elevator.IDLE:
-                e.load = [r for r in e.load if r != e.pos]
-                d = e.CAPACITY - len(e.load)
-                e.load += floors[e.pos][:d]
-                del floors[e.pos][:d]
-                idle(e)
+                e.re_load(self.floors[e.pos])
+                idle(e, self.floors)
 
-        move_elevators(elevators)
+        self.move_elevators()
 
-        for e in elevators:
+        for e in self.elevators:
             if e.pos == e.target:
                 e.state = Elevator.IDLE
             else:
-                e.state = Elevator.FETCH
+                if e.load:
+                    e.state = Elevator.SEND
+                else:
+                    e.state = Elevator.FETCH
 
-        generate_riders()
+        self.generate_riders()
 
-        # update display
-        for i, e in enumerate(elevators):
-            print_elevator(i, e.old_pos, c=' '*len(str(e)))
-            print_elevator(i, e.pos, c=str(e), color=e.state)
+    def update(self):
 
-        for f in xrange(len(floors)):
-            m = str(len(floors[f]))
-            s.print_at(m, frame_x+2, frame_y + frame_h - 2 - f)
+        self.update_state()
+        self.display.update_display(self.elevators, self.floors)
 
-        s.refresh()
 
-        d = SECOND - (time.time() - current_frame)
+def main(s):
+    floor_count = 20
+    elev_count = 4
+
+    g = Game(s, floor_count, elev_count)
+
+    start_time = time.time()
+
+    while time.time() < start_time + GAME_TIME:
+
+        current_frame = time.time()
+
+        g.update()
+
+        d = GAME_SECOND - (time.time() - current_frame)
         if d > 0:
             time.sleep(d)
 
 
-Screen.wrapper(main)
+try:
+    Screen.wrapper(main)
+except KeyboardInterrupt:
+    pass
+
+print 'Riders:', Rider.produced
+print 'Delivered', Rider.delivered
+print 'Average alive time:', Rider.time_alive / Rider.produced
+print 'Average ride time:', Rider.time_riding / Rider.delivered
